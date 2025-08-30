a483a8e745f57830f65cdadcbc9e508f79860fab
paarvathyammal <sponzorapp@gmail.com>
2025-08-30 23:07:46 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,11 @@
+import { initGA } from './ga-boot';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+
+// Initialize Google Analytics 4
+initGA('G-VYMWSN2S5W');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
